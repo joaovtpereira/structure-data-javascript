592d3de8269cd2b4902105a88fd0cd3d18f952de
joaovtpereira <joaovitorpessoal2@gmail.com>
2023-05-29 19:34:35 -0300

    decimal function with stack

---
+++ chapter4/decimalToBinary.js
@@ -0,0 +1,24 @@
+import Stack from "./stack.js";
+
+function decimalToBinary(decNumber) {
+    const remStack = new Stack();
+
+    let number = decNumber;
+    let rem;
+    let binaryString = '';
+
+    while(number > 0) {
+        rem = Math.floor(number%2);
+        remStack.push(rem);
+        number = Math.floor(number / 2);
+    }
+
+    while(!remStack.isEmpty()){
+        binaryString += remStack.pop().toString();
+    }
+
+    return binaryString;
+}
+
+const binary = decimalToBinary(10);
+console.log({binary})
--- chapter4/stack.js
+++ chapter4/stack.js
@@ -1,6 +1,6 @@
 export default class Stack {
     constructor() {
-        this.count() = 0;
+        this.count = 0;
         this.items = {};
     }
 
