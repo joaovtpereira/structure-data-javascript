10f1d3bd2ba632683c1b15b14ee2166003fd3f58
joaovtpereira <joaovitorpessoal2@gmail.com>
2023-07-06 13:46:56 -0300

    palindrome validator

---
+++ chapter5/teste_games.js
@@ -0,0 +1,8 @@
+import { palindromeChecker } from './validator_palindrome.js';
+
+console.log("a", palindromeChecker('a'))
+console.log("aa", palindromeChecker('aa'))
+console.log("abb", palindromeChecker('abb'))
+console.log("uvaavu", palindromeChecker('uvaavu'))
+
+
